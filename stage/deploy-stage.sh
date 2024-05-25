45f67b70a83e164348aad6484ed6fcdbc8d3caa0
c_Door in <egor1@mail.ru>
2024-05-25 13:10:12 +0300

    Update deploy-stage.sh

--- stage/deploy-stage.sh
+++ stage/deploy-stage.sh
@@ -7,6 +7,13 @@ sudo -v
 sudo mkdir -p /var/www/docker-star-burger-site
 sudo mkdir -p /var/www/docker-star-burger-site/staticfiles
 
+cd ../backend
+docker build --tag star-burger-backend .
+
+cd ../frontend
+docker build --tag star-burger-frontend .
+
+cd ../stage
 sudo docker compose up
 
 sudo systemctl reload nginx.service
